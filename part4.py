87ad5e642494de0a8b258358c928525bc64d6ae2
Jaclyn Cohen <jczetta@Jaclyns-MacBook.local>
2018-06-10 13:01:20 -0400

    Comment instrs release

--- part4.py
+++ part4.py
@@ -1,1 +1,6 @@
 # Imports -- you may add others but do not need to
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Code here should involve creation of the bar chart as specified in instructions
+# And opening / using the CSV file you created earlier with noun data from tweets
